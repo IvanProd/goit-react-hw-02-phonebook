ea46095c58bcef6cdc976708248253f02679bfd6
Ivan Prodeus <learning.webpractice@gmail.com>
2022-10-19 22:20:17 +0300

    3

--- src/components/Section/Section.styled.jsx
+++ src/components/Section/Section.styled.jsx
@@ -5,5 +5,5 @@ export const SectionBox = styled.section`
   width: 350px;
   border: 1px solid black;
   border-radius: 4px;
-  box-shadow: 1px 1px 25px 1px rgba(0, 0, 0, 0.55);
+  margin: 30px 0 15px 30px;
 `;
