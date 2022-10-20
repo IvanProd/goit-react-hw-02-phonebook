79b906c9bfc48b8d48bd20e4e90b1d9d2c393f0b
Ivan Prodeus <learning.webpractice@gmail.com>
2022-10-20 17:28:18 +0300

    corrected

--- src/components/App/App.jsx
+++ src/components/App/App.jsx
@@ -14,7 +14,6 @@ class App extends Component {
       { id: 'id-3', name: 'Eden Clements', number: '645-17-79' },
       { id: 'id-4', name: 'Annie Copeland', number: '227-91-26' },
     ],
-    filter: '',
   };
 
   handleSubmit = ({ name, number }) => {
@@ -49,7 +48,7 @@ class App extends Component {
   onFilteredContacts = () => {
     const contacts = this.state.contacts;
     return contacts.filter(contact =>
-      contact.name.toLowerCase().includes(this.state.filter)
+      contact.name.toLowerCase().trim()
     );
   };
 
